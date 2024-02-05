2aa855e79a7d5fa1c9f340e61904a7e37c5562e9
zhor <zhor@ThinkPad-L14-Gen-2-c94982d2.localdomain>
2024-02-05 21:22:06 +0300

    initial commit

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <main>
+    <div class="login">
+      <div class="login__header">
+        <h4>💰 Xpense Tracker</h4>
+      </div>
+      <div id="login">
+        <div class="login_svg">
+          <img src="@/assets/images/svg/login2.svg" alt="" />
+        </div>
+        <form class="form">
+          <div class="form-input">
+            <label>Enter Email</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div class="form-input">
+            <label>Enter Password</label>
+            <input type="password" name="password" id="password" />
+          </div>
+          <button type="submit">Login</button>
+          <p class="signup">Don't have an account, <a href="register.html">Sign Up</a></p>
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
