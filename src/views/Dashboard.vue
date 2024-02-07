22a3eedca6e9058704d73cae313ce841992ad5ef
zhor <zhor@ThinkPad-L14-Gen-2-c94982d2.localdomain>
2024-02-07 19:08:19 +0300

    add dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,36 +1,18 @@
 <script></script>
 <template>
-   <div>
+  <div>
     <div class="appbar">
-        <h4 class="appbar__title">💰 Xpense Tracker</h4>
+      <h4 class="appbar__title">💰 Expense Tracker</h4>
     </div>
-    <div class="expense">
-        <div id="app">
-            <form class="form">
-                <img class="expense-svg" src="@/assets/images/svg/wallet.svg" alt="">
-                <h1 class="expense-heading">
-                    Add Transaction.
-                </h1>
-                <div class="form-input">
-                    <label>Expense Name</label>
-                    <input type="text" name="expenseName" id="expenseName">
-                </div>
-                <div class="form-input">
-                    <label>Category</label>
-                    <select name="expenseCategory" id="expenseCategory">
-                        <option value=""></option>
-                    </select>
-                </div>
-                <div class="form-input">
-                    <label>Enter Amount</label>
-                    <input type="text" name="amount" id="amount">
-                </div>
-                <button type="submit" id="saveExpenseBtn">Save Expense</button>
-            </form>
-        </div>
-        <div class="footer">
-            <p>FarmSpace Media &copy 2021</p>
-        </div>
+    <div id="app">
+      <div class="expense__banner">
+        <h6 class="expense_title">Total Expenses</h6>
+        <h3 class="expense_total"><span class="currency">$</span>10,567.00</h3>
+      </div>
+      <div class="expense_categories">
+        <div class="expense"></div>
+      </div>
+      <button class="btn"><i class="fas fa-plus"></i></button>
     </div>
-   </div> 
+  </div>
 </template>
--- src/views/Expense.vue
+++ src/views/Expense.vue
@@ -1,36 +1,34 @@
 <script></script>
 <template>
-   <div>
+  <div>
     <div class="appbar">
-        <h4 class="appbar__title">💰 Xpense Tracker</h4>
+      <h4 class="appbar__title">💰 Xpense Tracker</h4>
     </div>
     <div class="expense">
-        <div id="app">
-            <form class="form">
-                <img class="expense-svg" src="@/assets/images/svg/wallet.svg" alt="">
-                <h1 class="expense-heading">
-                    Add Transaction.
-                </h1>
-                <div class="form-input">
-                    <label>Expense Name</label>
-                    <input type="text" name="expenseName" id="expenseName">
-                </div>
-                <div class="form-input">
-                    <label>Category</label>
-                    <select name="expenseCategory" id="expenseCategory">
-                        <option value=""></option>
-                    </select>
-                </div>
-                <div class="form-input">
-                    <label>Enter Amount</label>
-                    <input type="text" name="amount" id="amount">
-                </div>
-                <button type="submit" id="saveExpenseBtn">Save Expense</button>
-            </form>
-        </div>
-        <div class="footer">
-            <p>FarmSpace Media &copy; 2021</p>
-        </div>
+      <div id="app">
+        <form class="form">
+          <img class="expense-svg" src="@/assets/images/svg/wallet.svg" alt="" />
+          <h1 class="expense-heading">Add Transaction.</h1>
+          <div class="form-input">
+            <label>Expense Name</label>
+            <input type="text" name="expenseName" id="expenseName" />
+          </div>
+          <div class="form-input">
+            <label>Category</label>
+            <select name="expenseCategory" id="expenseCategory">
+              <option value=""></option>
+            </select>
+          </div>
+          <div class="form-input">
+            <label>Enter Amount</label>
+            <input type="text" name="amount" id="amount" />
+          </div>
+          <button type="submit" id="saveExpenseBtn">Save Expense</button>
+        </form>
+      </div>
+      <div class="footer">
+        <p>FarmSpace Media &copy; 2021</p>
+      </div>
     </div>
-   </div> 
+  </div>
 </template>
