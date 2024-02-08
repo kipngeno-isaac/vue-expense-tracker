0f9942b4bbb0973e7d64aae7b21030544c377aba
zhor <zhor@ThinkPad-L14-Gen-2-c94982d2.localdomain>
2024-02-08 19:49:26 +0300

    add register route

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,10 +1,10 @@
 <script></script>
 <template>
-  <div>
+  <main>
     <div class="appbar">
       <h4 class="appbar__title">💰 Expense Tracker</h4>
     </div>
-    <div id="app">
+    <div id="">
       <div class="expense__banner">
         <h6 class="expense_title">Total Expenses</h6>
         <h3 class="expense_total"><span class="currency">$</span>10,567.00</h3>
@@ -14,5 +14,5 @@
       </div>
       <button class="btn"><i class="fas fa-plus"></i></button>
     </div>
-  </div>
+  </main>
 </template>
